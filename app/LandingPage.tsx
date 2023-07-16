46cca80d5cee67951a6c8d06e13ac4adc2432d99
Joseph De Leon <joeseffdl@gmail.com>
2023-07-16 17:46:58 +0800

    Initial element on landing page
    To add animation

--- app/LandingPage.tsx
+++ app/LandingPage.tsx
@@ -1,7 +1,9 @@
 export default function LandingPage() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center p-24 bg-blue-300">
-      <div className="text-6xl font-bold">🚲 Biyahe ni Zeph 🚲</div>
+    <main className="flex h-screen flex-col items-center justify-center p-24 relative">
+      <div className="h-full w-1/5 absolute top-0 left-[20%] -skew-x-12 bg-blue-300"/>
+      <div className="h-full w-1/5 absolute top-0 right-[20%] -skew-x-12 bg-blue-700"/>
+      <div className="text-6xl font-bold z-10">🚲 Biyahe ni Zeph 🚲</div>
     </main>
-  );
+  )
 }
--- components/FlowingText.tsx
+++ components/FlowingText.tsx
@@ -4,7 +4,7 @@ type FlowingTextProps = {
 
 export default function FlowingText({ message }: FlowingTextProps) {
   return (
-    <main className="flex h-20 flex-col items-center justify-between p-24 bg-white">
+    <main className="flex h-20 flex-col items-center justify-between p-24 bg-white border-y-2 border-black">
       {message}
     </main>
   );
