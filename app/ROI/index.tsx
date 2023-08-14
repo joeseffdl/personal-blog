9fadc63eb3621c66d5ea18054ebe2c73b28fbb01
Joseph De Leon <joeseffdl@gmail.com>
2023-08-14 15:41:28 +0800

    Added interactivity to the table

--- app/ROI/index.tsx
+++ app/ROI/index.tsx
@@ -1,14 +1,15 @@
-import sampleData from "@/data/sample.json";
-import ROITable from "./ROITable";
+import { ROITable } from "./ROITable";
 import TableDescription from "./TableDescription";
+import { columns } from "./Columns";
+import { sampleData } from "./SampleData";
 
 export default function ROI() {
   const individualAmounts = sampleData.map((data) => data.amount)
   const totalSum = individualAmounts.reduce((sum, amount) => sum + amount, 0)
 
   return (
-    <main className="flex h-screen items-center justify-between p-24 bg-[#E8FCFF]">
-      <ROITable />
+    <main className="flex min-h-screen items-center justify-between p-24 bg-[#E8FCFF]">
+      <ROITable columns={columns} data={sampleData} />
       <TableDescription totalSum={totalSum} />
     </main>
   )
